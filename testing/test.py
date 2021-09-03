230ffb7eeeda68e96b6497f153c10cb6024fbc76
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2021-09-03 22:02:14 +0700

    Fix return value checking code

--- testing/test.py
+++ testing/test.py
@@ -11,9 +11,9 @@ for file in files:
     hasError = subprocess.run(
         [programPath, file], stdout=subprocess.DEVNULL, env=dict(os.environ, ASAN_OPTIONS="detect_leaks=1",
                                                                  ASAN_SYMBOLIZER_PATH="/usr/local/opt/llvm/bin/llvm"
-                                                                                      "-symbolizer")) != 0
+                                                                                      "-symbolizer")) == 1
     if hasError:
         print("Memory leak detect in test case %s" % file)
         exit(1)
     else:
-        print("Testcase %s completed." % file)
+        print("Testcase %s ok!" % file)
