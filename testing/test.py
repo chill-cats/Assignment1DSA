4d231ee2f0f575367ee120420565c2fe4507d991
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2021-09-03 21:30:19 +0700

    Add workflow for checking leak

--- testing/test.py
+++ testing/test.py
@@ -7,11 +7,13 @@ testPath = sys.argv[2]
 
 files = [os.path.join(testPath, f) for f in os.listdir(testPath) if os.path.isfile(os.path.join(testPath, f))]
 for file in files:
-    print("Running file %s" %file)
+    print("Running file %s" % file)
     hasError = subprocess.run(
         [programPath, file], stdout=subprocess.DEVNULL, env=dict(os.environ, ASAN_OPTIONS="detect_leaks=1",
                                                                  ASAN_SYMBOLIZER_PATH="/usr/local/opt/llvm/bin/llvm"
                                                                                       "-symbolizer")) != 0
     if hasError:
-        print("Memory leak detect in test case " + file)
+        print("Memory leak detect in test case %s" % file)
         exit(1)
+    else:
+        print("Testcase %s completed." % file)
