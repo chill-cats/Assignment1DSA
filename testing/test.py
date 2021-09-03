8eb038e8c966d5203fb355622a65645166c09dde
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2021-09-03 22:05:20 +0700

    Ouput test command on the same line

--- testing/test.py
+++ testing/test.py
@@ -7,7 +7,7 @@ testPath = sys.argv[2]
 
 files = [os.path.join(testPath, f) for f in os.listdir(testPath) if os.path.isfile(os.path.join(testPath, f))]
 for file in files:
-    print("Running file %s" % file)
+    # print("Running file %s" % file)
     hasError = subprocess.run(
         [programPath, file], stdout=subprocess.DEVNULL, env=dict(os.environ, ASAN_OPTIONS="detect_leaks=1",
                                                                  ASAN_SYMBOLIZER_PATH="/usr/local/opt/llvm/bin/llvm"
@@ -16,4 +16,4 @@ for file in files:
         print("Memory leak detect in test case %s" % file)
         exit(1)
     else:
-        print("Testcase %s ok!" % file)
+        print("Testcase %s ok!" % file, end='\r', flush=True)
