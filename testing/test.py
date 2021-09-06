891e84868e2fd1e87f36857907c2f0fb56e017c0
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2021-09-06 18:18:44 +0700

    Change test again because of false possitive

--- testing/test.py
+++ testing/test.py
@@ -11,7 +11,7 @@ for file in files:
     hasError = subprocess.run(
         [programPath, file], stdout=subprocess.DEVNULL, env=dict(os.environ, ASAN_OPTIONS="detect_leaks=1",
                                                                  ASAN_SYMBOLIZER_PATH="/usr/local/opt/llvm/bin/llvm"
-                                                                                      "-symbolizer")) != 0
+                                                                                      "-symbolizer")) == 1
     if hasError:
         print("Memory leak detect in test case %s" % file)
         exit(1)
