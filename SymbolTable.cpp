600a618633a9e4ffb03c0d06f9f0843269731a72
MKKK2505 <khanh.nguyen2552002@hcmut.edu.vn>
2021-09-20 10:13:41 +0700

    help

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -2,33 +2,9 @@
 
 using namespace std;
 
-int SymbolTable::count_line(string filename) {
-    int count = 0;
-    ifstream myfile;
-    myfile.open(filename);
-    if (myfile.is_open()) {
-        string s;
-        while (getline(myfile, s)) {
-            count++;
-        }
-    }
-    myfile.close();
-    return count;
-}
-
-void SymbolTable::destroy_lisst(DLinkedlisst &lisst) {
-    identifier_node *temp = lisst.head;
-    while (temp != nullptr) {
-        auto toBeDeletedNode = temp;
-        temp = temp->next;
-        delete toBeDeletedNode;
-    }
-}
-
 void SymbolTable::run(const string& filename) {
-    DLinkedlisst lisst;
-    int level = 0, count_begin = 0, count_end = 0, line = count_line(filename),
-            count_line = 0;
+    DLinkedLisst lisst;
+    int level = 0;
     regex insert(R"(^INSERT[ ]([a-z]\w*)[ ](string|number)$)");
     regex assign(R"(^ASSIGN[ ]([a-z]\w*)[ ]([a-z]\w*|\d+|'[a-zA-Z\d ]*')$)");
     regex begin(R"(^BEGIN$)");
@@ -42,99 +18,83 @@ void SymbolTable::run(const string& filename) {
         string s;
         while (getline(myfile, s)) {
             smatch data;
-            count_line++;
+
             if (regex_match(s, data, insert)) {
-                identifier_name newID(data.str(1), data.str(2), "", level);
-                if (insert_lisst(lisst, newID)) {
-                    lisst.size++;
+                identifier_name newID(data[1], data[2], "", level);
+                if (lisst.insert_lisst(newID)) {
                     cout << "success" << endl;
                 } else {
-                    destroy_lisst(lisst);
                     throw Redeclared(s);
                 }
+
             } else if (regex_match(s, data, assign)) {
-                string as = assign_lisst(lisst, data[1], data[2]);
+                string as = lisst.assign_lisst(data[1], data[2]);
                 if (as == "success") {
                     cout << "success" << endl;
                 } else if (as == "mismatch") {
-                    destroy_lisst(lisst);
                     throw TypeMismatch(s);
                 } else if (as == "undeclared") {
-                    destroy_lisst(lisst);
                     throw Undeclared(s);
                 } else if (as == "invalid") {
-                    destroy_lisst(lisst);
                     throw InvalidInstruction(s);
                 }
+
             } else if (regex_match(s, begin)) {
                 level++;
-                count_begin++;
+
             } else if (regex_match(s, end)) {
-                count_end++;
-                if (count_end > count_begin) {
-                    destroy_lisst(lisst);
+                if (level == 0) {
                     throw UnknownBlock();
                 }
-                end_lisst(lisst, level);
+                lisst.end_block(level);
                 level--;
+
             } else if (regex_match(s, print)) {
-                print_lisst(lisst);
+                DLinkedLisst::print_lisst(lisst.delete_same_ID());
+
             } else if (regex_match(s, rprint)) {
-                rprint_lisst(lisst);
+                DLinkedLisst::rprint_lisst(lisst.delete_same_ID());
+
             } else if (regex_match(s, data, lookup)) {
-                if (lookup_lisst(lisst, data[1]) == -1) {
-                    destroy_lisst(lisst);
+                if (lisst.lookup_lisst(data[1]) == -1) {
                     throw Undeclared(s);
                 } else {
-                    cout << lookup_lisst(lisst, data[1]) << endl;
+                    cout << lisst.lookup_lisst(data[1]) << endl;
                 }
+
             } else {
-                destroy_lisst(lisst);
                 throw InvalidInstruction(s);
             }
         }
-        if (count_line == line) {
-            if (count_begin > count_end) {
-                destroy_lisst(lisst);
-                throw UnclosedBlock(level);
-            }
+        if (level != 0) {
+            throw UnclosedBlock(level);
         }
     }
     myfile.close();
-    destroy_lisst(lisst);
 }
 
-bool SymbolTable::insert_lisst(DLinkedlisst &lisst,
-                               const identifier_name &element) {
-    auto *h1 = new identifier_node;
-    h1->data = element;
-    if (lisst.head == nullptr) {
-        lisst.head = h1;
-        h1->next = nullptr;
-        h1->prev = nullptr;
-        lisst.tail = lisst.head;
+bool DLinkedLisst::insert_lisst(const identifier_name& newID) {
+    auto *new_ID = new identifier_node;
+    new_ID->data = newID;
+    if (this->head == nullptr) {
+        this->head = new_ID;
+        this->tail = new_ID;
         return true;
     }
-    auto *h = lisst.head;
-    while (h->next != nullptr) {
-        if (h->data.ID == element.ID && h->data.level == element.level) {
-            delete h1;
+
+    for (auto *h = this->head; h != nullptr; h = h->next) {
+        if (h->data.ID == newID.ID && h->data.level == newID.level) {
+            delete new_ID;
             return false;
         }
-        h = h->next;
     }
-    if (h->data.ID == element.ID && h->data.level == element.level) {
-        delete h1;
-        return false;
-    }
-    h->next = h1;
-    h1->next = nullptr;
-    h1->prev = h;
-    lisst.tail = h1;
+    this->tail->next = new_ID;
+    new_ID->prev = this->tail;
+    this->tail = new_ID;
     return true;
 }
 
-string SymbolTable::type_of_value(string value) {
+string DLinkedLisst::type_of_value(const string& value) {
     regex number(R"(\d+)");
     regex string(R"('[a-zA-Z\d ]*')");
     regex id(R"([a-z]\w*)");
@@ -147,14 +107,11 @@ string SymbolTable::type_of_value(string value) {
     return "invalid";
 }
 
-string SymbolTable::assign_lisst(DLinkedlisst &lisst, const string &ID,
-                           const string &value) {
-    auto *h = lisst.tail;
-    while (h) {
+string DLinkedLisst::assign_lisst(const string& ID, const string& value) const {
+    for (auto *h = this->tail; h != nullptr; h = h->prev) {
         if (h->data.ID == ID) {
             if (type_of_value(value) == "id") {
-                auto *h1 = lisst.tail;
-                while (h1) {
+                for (auto *h1 = this->tail; h1 != nullptr; h1 = h1->prev) {
                     if (h1->data.ID == value) {
                         if (h1->data.type == h->data.type) {
                             h->data.value = value;
@@ -163,7 +120,6 @@ string SymbolTable::assign_lisst(DLinkedlisst &lisst, const string &ID,
                             return "mismatch";
                         }
                     }
-                    h1 = h1->prev;
                 }
                 return "undeclared";
             }
@@ -174,148 +130,91 @@ string SymbolTable::assign_lisst(DLinkedlisst &lisst, const string &ID,
                 return "mismatch";
             }
         }
-        h = h->prev;
     }
     return "undeclared";
 }
 
-void SymbolTable::end_lisst(DLinkedlisst &lisst, int level) {
-    if (lisst.head == nullptr)
+void DLinkedLisst::end_block(int level) {
+    if (this->head == nullptr) {
         return;
-
-    auto *h = lisst.head;
-    while (h) {
-        if (h->data.level == level) {
-            if (lisst.head == lisst.tail) {
-                delete lisst.head;
-                lisst.tail = nullptr;
-                lisst.head = nullptr;
-
-                return;
-            }
-            if (h == lisst.head) {
-                lisst.head = lisst.head->next;
-                delete h;
-                lisst.head->prev = nullptr;
-                h = lisst.head;
-            } else if (h == lisst.tail) {
-                lisst.tail = lisst.tail->prev;
-                lisst.tail->next = nullptr;
-                delete h;
-                break;
-            } else {
-                auto *h2 = h->prev;
-                h2->next = h->next;
-                h->next->prev = h2;
-                delete h;
-                h = h2->next;
-            }
-        } else {
-            h = h->next;
+    }
+    auto *h = this->tail;
+    while (h->data.level == level) {
+        if (this->head == this->tail) {
+            delete h;
+            this->head = nullptr;
+            this->tail = nullptr;
+            return;
         }
+        h = h->prev;
+        delete this->tail;
+        this->tail = h;
+        this->tail->next = nullptr;
     }
 }
 
-int SymbolTable::count_ID(DLinkedlisst lisst, const string &ID) {
-    if (lisst.head == nullptr)
-        return 0;
-    auto *h = lisst.head;
-    int count = 0;
-    while (h) {
-        if (h->data.ID == ID)
-            count++;
-        h = h->next;
+DLinkedLisst DLinkedLisst::delete_same_ID() {
+    DLinkedLisst lisst1(this);
+    if (this->head == nullptr) {
+        return lisst1;
     }
-    return count;
-}
 
-SymbolTable::DLinkedlisst SymbolTable::copy_lisst(DLinkedlisst lisst) {
-    if (lisst.head == nullptr)
-        return lisst;
-    DLinkedlisst lisst1;
-    auto *h = lisst.head;
-    while (h) {
-        identifier_name h1(h->data.ID, h->data.type, h->data.value, h->data.level);
-        insert_lisst(lisst1, h1);
-        h = h->next;
+    for (auto *h = lisst1.tail; h != nullptr; h = h->prev) {
+        for (auto *h1 = h->prev; h1 != nullptr; h1 = h1->prev) {
+            if (h1->data.ID == h->data.ID) {
+                if (h1 == lisst1.head) {
+                    lisst1.head = lisst1.head->next;
+                    lisst1.head->prev = nullptr;
+                    delete h1;
+                    break;
+                }
+                auto *h2 = h1;
+                h1 = h1->next;
+                h1->prev = h2->prev;
+                h2->prev->next = h1;
+                delete h2;
+            }
+        }
     }
     return lisst1;
 }
 
-void SymbolTable::delete_same_ID(DLinkedlisst &lisst) {
-    if (lisst.head == nullptr)
+void DLinkedLisst::print_lisst(const DLinkedLisst& lisst) {
+    if (lisst.head == nullptr) {
         return;
-    auto *h = lisst.head;
-    while (h) {
-        int a = count_ID(lisst, h->data.ID);
-        if (a > 1) {
-            auto *h1 = h;
-            string cur = h->data.ID;
-            while (a > 1) {
-                if (h1->data.ID == cur) {
-                    if (h == lisst.head) {
-                        h1 = h1->next;
-                        lisst.head = lisst.head->next;
-                        delete h;
-                        lisst.head->prev = nullptr;
-                        h = h1;
-                        a--;
-                    } else {
-                        h1 = h1->next;
-                        auto *h2 = h->prev;
-                        h2->next = h->next;
-                        h->next->prev = h2;
-                        delete h;
-                        h = h1;
-                        a--;
-                    }
-                }
-                h1 = h1->next;
-                if (h1 == nullptr)
-                    break;
-                h = h1;
-            }
-        }
-        h = h->next;
     }
-}
 
-void SymbolTable::print_lisst(DLinkedlisst lisst) {
-    if (lisst.head == nullptr)
-        return;
-    DLinkedlisst lisst1 = copy_lisst(lisst);
-    delete_same_ID(lisst1);
-    auto h = lisst1.head;
+    auto h = lisst.head;
     while (h->next) {
         cout << h->data.ID << "//" << h->data.level << " ";
         h = h->next;
     }
     cout << h->data.ID << "//" << h->data.level;
     cout << endl;
-    destroy_lisst(lisst1);
 }
 
-void SymbolTable::rprint_lisst(DLinkedlisst lisst) {
-    if (lisst.head == nullptr)
+void DLinkedLisst::rprint_lisst(const DLinkedLisst& lisst) {
+    if (lisst.head == nullptr) {
         return;
-    DLinkedlisst lisst1 = copy_lisst(lisst);
-    delete_same_ID(lisst1);
-    auto h = lisst1.tail;
+    }
+
+    auto h = lisst.tail;
     while (h->prev) {
         cout << h->data.ID << "//" << h->data.level << " ";
         h = h->prev;
     }
     cout << h->data.ID << "//" << h->data.level;
     cout << endl;
-    destroy_lisst(lisst1);
 }
 
-int SymbolTable::lookup_lisst(DLinkedlisst lisst, const string &ID) {
-    identifier_node *h = lisst.tail;
-    while (h) {
-        if (h->data.ID == ID)
+int DLinkedLisst::lookup_lisst(const string& ID) const {
+    if (this->head == nullptr) {
+        return -1;
+    }
+    for (auto *h = this->tail; h != nullptr; h = h->prev) {
+        if (h->data.ID == ID) {
             return h->data.level;
-        h = h->prev;
+        }
     }
     return -1;
 }
--- SymbolTable.h
+++ SymbolTable.h
@@ -4,61 +4,72 @@
 
 using namespace std;
 
+class identifier_name {
+public:
+    identifier_name(): ID(), type(), value(""), level(0) {}
+    identifier_name(string ID, string type, string value, int level): ID(ID), type(type), value(value), level(level) {}
+    string ID;
+    string type;
+    string value;
+    int level;
+};
+
+class identifier_node {
+public:
+    identifier_node(): data(identifier_name()), next(nullptr), prev(nullptr) {}
+    identifier_node(identifier_name data, identifier_node* next, identifier_node* prev): data(data), next(next), prev(nullptr) {}
+
+    identifier_name data;
+    identifier_node* next;
+    identifier_node* prev;
+};
+
+class DLinkedLisst {
+public:
+    identifier_node* head;
+    identifier_node* tail;
+
+public:
+    DLinkedLisst(): head(nullptr), tail(nullptr) {}
+
+    DLinkedLisst(DLinkedLisst *other) {
+        for (auto *h = other->head; h != nullptr; h = h->next) {
+            identifier_name h1(h->data.ID, h->data.type, h->data.value, h->data.level);
+            insert_lisst(h1);
+        }
+    }
+
+    ~DLinkedLisst() {
+        identifier_node *temp = this->head;
+        while (temp != nullptr) {
+            auto toBeDeletedNode = temp;
+            temp = temp->next;
+            delete toBeDeletedNode;
+        }
+    }
+
+public:
+    bool insert_lisst(const identifier_name& newID);
+
+    static string type_of_value(const string& value);
+
+    string assign_lisst(const string& ID, const string& value) const;
+
+    void end_block(int level);
+
+    DLinkedLisst delete_same_ID();
+
+    static void print_lisst(const DLinkedLisst& lisst) ;
+
+    static void rprint_lisst(const DLinkedLisst& lisst) ;
+
+    int lookup_lisst(const string& ID) const;
+
+};
+
 class SymbolTable {
 public:
-    SymbolTable() {}
-    void run(const string& filename);
-
-    struct identifier_name {
-        identifier_name(): ID(), type(), value(""), level(0) {}
-        identifier_name(string ID, string type, string value, int level): ID(ID), type(type), value(value), level(level) {}
-        string ID;
-        string type;
-        string value;
-        int level;
-    };
-
-    struct identifier_node {
-        identifier_node(): data(identifier_name()), next(nullptr) {}
-        identifier_node(identifier_name data, identifier_node* next): data(data), next(next) {}
-
-        identifier_name data;
-        identifier_node* next;
-        identifier_node* prev;
-    };
-
-    struct DLinkedlisst{
-        DLinkedlisst(): head(nullptr), tail(nullptr), size(0) {}
-        DLinkedlisst(identifier_node* head, identifier_node* tail, int size): head(head), tail(tail), size(size) {}
-
-        identifier_node* head;
-        identifier_node* tail;
-        int size;
-    };
-
-    int count_line(string filename);
-
-    bool insert_lisst(DLinkedlisst &lisst, const identifier_name& newID);
-
-    string type_of_value(string value);
-
-    string assign_lisst(DLinkedlisst &lisst, const string& ID, const string& value);
-
-    void end_lisst(DLinkedlisst &lisst, int level);
-
-    int count_ID(DLinkedlisst lisst, const string& ID);
-
-    void print_lisst(DLinkedlisst lisst);
-
-    void rprint_lisst(DLinkedlisst lisst);
-
-    int lookup_lisst(DLinkedlisst lisst, const string& ID);
-
-    DLinkedlisst copy_lisst(DLinkedlisst);
-
-    void delete_same_ID(DLinkedlisst &lisst);
-
-    void destroy_lisst(DLinkedlisst &lisst);
+    static void run(const string& filename);
 };
 
 #endif
